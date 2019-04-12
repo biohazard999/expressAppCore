acd6bf6cae398b030418a9d022c03fb3599772fd
Manuel Grundner <m.grundner@delegate.at>
2019-04-12 08:51:58 +0200

    Add project files.

---
+++ src/expressAppCore.Win/Program.cs
@@ -0,0 +1,31 @@
+﻿using DevExpress.ExpressApp.Win;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expressAppCore.Win
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var application = new ExpressAppCoreWinApplication();
+
+            application.Setup();
+            application.Start();
+        }
+    }
+
+    public class ExpressAppCoreWinApplication : WinApplication
+    {
+
+    }
+}
